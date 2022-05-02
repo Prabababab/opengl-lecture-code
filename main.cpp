2382679711b2aa35ee6b9b3c37a5e9f1b024f214
Prabhvir Babra <theprabab@gmail.com>
2022-05-01 22:49:11 -0700

    Create a Black Window

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "extern/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const int width = 800, height = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(width, height, "Black Window!", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to Create a Window :(\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
