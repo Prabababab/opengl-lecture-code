b4f7a28b4a6515862f7f836c1dd7bb6d34f6d5f5
Prabhvir Babra <theprabab@gmail.com>
2022-05-05 06:22:56 -0700

    Implement more interesting window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "extern/glad/glad.h"
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -13,8 +14,8 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window =
-      glfwCreateWindow(width, height, "Black Window!", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(
+      width, height, "Not so-Black and Dull Window", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to Create a Window :(\n";
     glfwTerminate();
@@ -32,8 +33,18 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   while (!glfwWindowShouldClose(window)) {
+    // Set a new color for OpenGL to clear the screen with
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
     glfwSwapBuffers(window);
+
+    // Poll for Input from the keyboard
     glfwPollEvents();
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
+
+    // Clear the Screen with the color set earlier
+    glClear(GL_COLOR_BUFFER_BIT);
   }
 
   glfwTerminate();
